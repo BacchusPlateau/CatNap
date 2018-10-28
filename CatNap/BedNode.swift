bc14ce8ad55082009a54b9f543e4e692b2f8ec71
Bret Williams <bret@brets-mbp.tx.rr.com>
2018-10-28 13:27:28 -0500

    Beginning physics

---
+++ CatNap/BedNode.swift
@@ -0,0 +1,15 @@
+//
+//  BedNode.swift
+//  CatNap
+//
+//  Created by Bret Williams on 10/28/18.
+//  Copyright © 2018 Bret Williams. All rights reserved.
+//
+
+import SpriteKit
+
+class BedNode : SKSpriteNode {
+    
+    
+    
+}
